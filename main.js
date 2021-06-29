bb68d165ceeb16c887fec97469cf29aec65606e8
Agujerín <argentinavencera@hotmail.com>
2021-06-29 15:31:27 -0300

    Primera versión

--- main.js
+++ main.js
@@ -1,102 +1,86 @@
-/*CREACIÓN DEL SELECT
+"use strict"
 
-const select = document.getElementById("select")
+//VARIABLES Y FUNCIONES GENERALES
+const speed = ['1/500s','1/250s','1/125s','1/60s','1/30s','1/15s','1/8s','1/4s','.5s','1s','2s','4s','8s','15s','30s','1m','2m','4m','8m','15m','30m','1h','2h','4h','8h','15h']
 
-const fragmento = document.createDocumentFragment()
+const diaphragm = ['1.4','2','2.8','4','5.6','8','11','16','22','32','45','64','90','128','180','256','360','512','720','1024']
 
-let options = ['Otros','1/4s.','1/8s.','1/15s.','1/30s.','1/60','1/60s.','8m.','15m.','15m.','1h.','2h.','4h.','8h','15h.']
+let values = []
 
-for (option of options){
-    let item = document.createElement("OPTION")
-    item.innerHTML = option
-    fragmento.appendChild(item)
+const selection = (container,value)=>{
+    container.classList.add("selected-item")
+    container.innerText = `${value}`
 }
 
-select.appendChild(fragmento)
-*/
-
-//GENERALES
-
-let velocidades = ['1/500s','1/250s','1/125s','1/60s','1/30s','1/15s','1/8s','1/4s','.5s','1s','2s','4s','8s','15s','30s','1m','2m','4m','8m','15m','30m','1h','2h','4h','8h','15h']
-let diafragmas = ['1.4','2','2.8','4','5.6','8','11','16','22','32','45','64','90','128','180','256','360','512','720','1024']
-
-const item = (a,b)=>{
-    a.classList.add ("item")
-    a.innerText = `${b}`
+const valueSelector = (e,array,container)=>{
+    let value = e.target.innerText
+    
+    if (array.indexOf(value) == -1){
+        console.log ("error")
+    }else {
+        values.push(array.indexOf(value))
+        selection(container,value)
+    }
 }
 
 
 //SELECCIÓN DE DIAFRAGMA FOTÓMETRO
+const container1 = document.getElementById("container1")
 
-const cont1 = document.getElementById ("contenedor1")
 
-let valores = []
-
-cont1.addEventListener ("click",(e)=>{
-    if (valores.length != 0) location.reload()
-    let valor = e.target.innerText
-        if (diafragmas.indexOf(valor)== -1){
-            console.log ("error")
-        } else {
-            valores.push(diafragmas.indexOf(valor))
-            item (cont1,valor)
-            }
+container1.addEventListener("click",(e)=>{
+    if (values.length != 0) location.reload()
+    
+    valueSelector(e,diaphragm,container1)
 })
+
 
 //SELECCIÓN DE VELOCIDAD FOTÓMETRO
+const container2 = document.getElementById("container2")
 
-const cont2 = document.getElementById ("contenedor2")
-
-cont2.addEventListener ("click",(e)=>{
-    if (valores.length != 1) location.reload()
-    let valor = e.target.innerText
-        if (velocidades.indexOf(valor)== -1){
-            console.log ("error")
-        } else {
-            valores.push(velocidades.indexOf(valor))
-            item (cont2,valor)
-            }
+container2.addEventListener("click",(e)=>{
+    if (values.length != 1) location.reload()
+    
+    valueSelector(e,speed,container2)
 })
+
 
 //SELECCIÓN DE DIAFRAGMA DE CÁMARA
+const container3 = document.getElementById("container3")
 
-const cont3 = document.getElementById ("contenedor3")
-
-cont3.addEventListener ("click",(e)=>{
-    let valor = e.target.innerText
-        if (diafragmas.indexOf(valor)== -1){
-            console.log ("error")
-        } else valores.push(diafragmas.indexOf(valor))
+container3.addEventListener("click",(e)=> {
+    if (values.length != 2) location.reload()
+    
+    valueSelector(e,diaphragm,container3)
 })
+
 
 //CONFIRMACIÓN Y RESULTADO
+const confirmationBtn = document.getElementById("confirmationBtn")
+const resultMsg = document.getElementById("resultMsg")
+const resultText = document.getElementById("resultText")
+const resultBtn = document.getElementById("resultBtn")
 
-const button = document.getElementById ("button")
-const result = document.getElementById ("result")
-const p = document.getElementById ("text")
-const btn = document.getElementById ("btn")
-const all = document.getElementById ("all")
+const transition=()=>{
+    resultMsg.classList.add("show-result")
+    
+    resultBtn.addEventListener("click",()=>{
+        resultMsg.classList.remove("show-result")       
+        location.reload()
+    })
+}
 
-const transition = (()=>{
-    result.classList.add ("showResult")
-    all.classList.add ("all") 
-            btn.addEventListener ("click",()=>{
-                result.classList.remove ("showResult")       
-                all.classList.remove ("all")
-                location.reload()
-            })
-})
-
-button.addEventListener ("click",()=>{
-    let num1 = valores[2] - valores[0]
-    let num2 = valores[1] + num1
-    let num3= velocidades[num2]
-        if (num3 == undefined){
-            p.innerHTML = "La velocidad correspondiente no existe en nuestro contador"
-            transition()
-        } else {
-            p.innerHTML = `Para su cámara con diafragma ${diafragmas[valores[2]]} la velocidad es de ${num3}`
-            transition()
-        }
+confirmationBtn.addEventListener ("click",()=>{
+    let select1 = values[2] - values[0]
+    let select2 = values[1] + select1
+    let select3= speed[select2]
+    
+    if (select3 == undefined){
+        resultText.innerHTML = "La exposición correspondiente no existe en nuestro contador"
+        transition()
+    }else {
+        resultText.innerHTML = `Para N° F ${diaphragm[values[2]]} la exposición es: ${select3}`
+        transition()
+    }
 })
 
